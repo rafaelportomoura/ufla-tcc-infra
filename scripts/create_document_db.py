5414c887064edbc5db5ebda0d5e92c307c0060f3
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-07 12:39:19 -0300

    refactor: Create document DB stack and deploy it

---
+++ scripts/create_document_db.py
@@ -0,0 +1,46 @@
+from stacks import document_db
+from scripts.exception import DeployException
+from scripts.cloudformation import CloudFormation
+from scripts.args import get_args
+
+
+args = get_args(
+    {
+        "stage": {"type": "str", "required": False, "default": "prod"},
+        "tenant": {"type": "str", "required": False, "default": "tcc"},
+        "region": {"type": "str", "required": False, "default": "us-east-2"},
+        "profile": {"type": "str", "required": False, "default": "default"},
+        "log_level": {"type": "int", "required": False, "default": 3},
+    }
+)
+
+stage = args["stage"]
+tenant = args["tenant"]
+region = args["region"]
+profile = args["profile"]
+log_level = args["log_level"]
+cloudformation = CloudFormation(profile=profile, region=region, log_level=log_level)
+
+
+################################################
+exports = cloudformation.list_exports()
+VPC_ID = cloudformation.get_export_value(exports, f"{stage}-{tenant}-vpc-id")
+PRIVATE_SUBNETS_IDS = cloudformation.get_export_value(
+    exports, f"{stage}-{tenant}-private-subnets-list"
+)
+PRIVATE_SECURITY_GROUPS = cloudformation.get_export_value(
+    exports, f"{stage}-{tenant}-private-subnet-security-group-id"
+)
+################################################
+#  🚀 DCOUMENT DB DEPLOY
+################################################
+DOCUMENT_DB = document_db.stack(
+    stage=stage,
+    tenant=tenant,
+    subnet_ids_list=PRIVATE_SUBNETS_IDS,
+    security_group_id_list=PRIVATE_SECURITY_GROUPS,
+)
+cloudformation.deploy_stack(DOCUMENT_DB)
+
+if not cloudformation.stack_is_succesfully_deployed(DOCUMENT_DB["stack_name"]):
+    raise DeployException(DOCUMENT_DB)
---
+++ scripts/stacks/document_db.py
@@ -0,0 +1,21 @@
+from stacks.template_path import path
+from scripts.stacks import Stack, stack_name
+
+
+def my_stack_name(stage: str, tenant: str) -> str:
+    return stack_name(stage=stage, tenant=tenant, name="DocumentDb")
+
+
+def stack(
+    stage: str, tenant: str, subnet_ids_list: str, security_group_id_list: str
+) -> Stack:
+    return Stack(
+        template=path("vpc", "vpc.yaml"),
+        stack_name=my_stack_name(stage, tenant),
+        parameters={
+            "Stage": stage,
+            "Tenant": tenant,
+            "SubnetIdsList": subnet_ids_list,
+            "SecurityGroupIdsList": security_group_id_list,
+        },
+    )
