07964aad91d60a8ee3f2ad8efc08fbf3f260d43b
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-08 17:59:36 -0300

    refactor: Update create_us_east_1_certificate.py to allow specifying region as a parameter

--- scripts/create_us_east_1_certificate.py
+++ scripts/create_us_east_1_certificate.py
@@ -8,6 +8,7 @@ args = get_args(
     {
         "stage": {"type": "str", "required": False, "default": "prod"},
         "tenant": {"type": "str", "required": False, "default": "tcc"},
+        "region": {"type": "str", "required": False, "default": "us-east-2"},
         "profile": {"type": "str", "required": False, "default": "default"},
         "log_level": {"type": "int", "required": False, "default": 3},
     }
@@ -15,7 +16,7 @@ args = get_args(
 
 stage = args["stage"]
 tenant = args["tenant"]
-region = "us-east-1"
+region = args["region"]
 profile = args["profile"]
 log_level = args["log_level"]
 cloudformation = CloudFormation(profile=profile, region=region, log_level=log_level)
@@ -31,11 +32,12 @@ DOMAIN_NAME = cloudformation.get_export_value(exports, f"{stage}-{tenant}-domain
 CERTIFICATE_STACK = certificate.stack(
     stage=stage, tenant=tenant, hosted_zone=HOSTED_ZONE_ID, domain_name=DOMAIN_NAME
 )
-cloudformation.deploy_stack(CERTIFICATE_STACK)
-exports = cloudformation.list_exports()
-CERTIFICATE = cloudformation.get_export_value(
+us_east_1 = CloudFormation(profile=profile, region=region, log_level=log_level)
+us_east_1.deploy_stack(CERTIFICATE_STACK)
+exports = us_east_1.list_exports()
+CERTIFICATE = us_east_1.get_export_value(
     exports, f"{stage}-{tenant}-domain-certificate"
 )
 print(CERTIFICATE)
-if not cloudformation.stack_is_succesfully_deployed(CERTIFICATE_STACK["stack_name"]):
+if not us_east_1.stack_is_succesfully_deployed(CERTIFICATE_STACK["stack_name"]):
     raise DeployException(CERTIFICATE_STACK)
