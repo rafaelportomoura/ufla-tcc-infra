5d8d1da9f6e948360d30590e0166cdacebe22d73
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-09 10:57:03 -0300

    refactor: Remove AWS API Gateway custom domain stack and related code

--- scripts/create_certificate.py
+++ scripts/create_certificate.py
@@ -39,26 +39,6 @@ CERTIFICATE_STACK = certificate.stack(
     stage=stage, tenant=tenant, hosted_zone=HOSTED_ZONE_ID, domain_name=DOMAIN_NAME
 )
 us_east_1.deploy_stack(CERTIFICATE_STACK)
-exports = us_east_1.list_exports()
-CERTIFICATE = us_east_1.get_export_value(
-    exports, f"{stage}-{tenant}-domain-certificate"
-)
 
 if not us_east_1.stack_is_succesfully_deployed(CERTIFICATE_STACK["stack_name"]):
     raise DeployException(CERTIFICATE_STACK)
-################################################
-# 🚀 API GATEWAY DOMAIN
-################################################
-API_GATEWAY_DOMAIN_STACK = api_gateway_domain.stack(
-    stage=stage,
-    tenant=tenant,
-    domain_name=DOMAIN_NAME,
-    hosted_zone=HOSTED_ZONE_ID,
-    certificate=CERTIFICATE,
-)
-cloudformation.deploy_stack(API_GATEWAY_DOMAIN_STACK)
-
-if not cloudformation.stack_is_succesfully_deployed(
-    API_GATEWAY_DOMAIN_STACK["stack_name"]
-):
-    raise DeployException(API_GATEWAY_DOMAIN_STACK)
