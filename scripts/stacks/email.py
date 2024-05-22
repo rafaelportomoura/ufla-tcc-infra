0b1c23ba7bfcc7893f0f5ecc46f5d15fb8bcf29c
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-22 19:34:37 -0300

    fix: stack name

--- scripts/stacks/email.py
+++ scripts/stacks/email.py
@@ -2,8 +2,8 @@ from stacks.template_path import path
 from scripts.stacks import Stack, stack_name
 
 
-def my_stack_name(email: str) -> str:
-    return email
+def my_stack_name() -> str:
+    return "EmailIdentityStack-Deploy"
 
 
 def stack(
@@ -11,7 +11,7 @@ def stack(
 ) -> Stack:
     return Stack(
         template=path("ses", "email_identity.yaml"),
-        stack_name=my_stack_name(email=email),
+        stack_name=my_stack_name(),
         parameters={
             "Email": email,
         },
